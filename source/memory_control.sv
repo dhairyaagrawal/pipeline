078fd821b18410a08738558f67dbb6097613e8ca
Dhairya Agrawal <agrawad@purdue.edu>
2018-09-05 17:54:28 -0400

    added some text to memctrl

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -21,5 +21,35 @@ module memory_control (
 
   // number of cpus for cc
   parameter CPUS = 2;
+  
+  always_ff@(posedge CLK, negedge nRST) begin
+    if(!nRST) begin
+      ccif.iload <= '0;
+      ccif.dload <= '0;
+    end else if(ccif.dREN & ccif.ramstate == ACCESS) begin
+      ccif.dload <= ccif.ramload;
+      ccif.dwait = 1'b0;
+      ccif.ramREN = 1'b0;
+    end else if(ccif.iREN & ccif.ramstate == ACCESS) begin
+      ccif.iload <= ccif.ramload;
+      ccif.iwait = 1'b0;
+      ccif.ramREN = 1'b0;
+    end
+  end
 
+  always_comb begin
+    ccif.ramWEN = 1'b0;
+    ccif.ramREN = 1'b0;
+    ccif.ramaddr = '0;
+    ccif.ramstore = '0;
+    if(ccif.dREN) begin
+      ccif.ramaddr = ccif.daddr;
+      ccif.ramREN = 1'b1;
+
+      ccif.iwait = 1'b1;
+      ccif.dwait = 1'b1;
+    end else if(ccif.iREN) begin
+      ccif.ramaddr = ccif.iaddr;
+      ccif.ramREN = 1'b1;
+    end  
 endmodule
